dda449d571529fd30ef9b36222142debce82bffc
Pooja Kelkar <poojakelkar53@gmail.com>
2020-10-15 21:24:27 +0530

    refactor

--- core/views.py
+++ core/views.py
@@ -4,5 +4,26 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 
 
+def get_html_content():
+        import requests
+        USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+        LANGUAGE = "en-US,en;q=0.5"
+        session = requests.Session()
+        session.headers['User-Agent'] = USER_AGENT
+        session.headers['Accept-Language'] = LANGUAGE
+        session.headers['Content-Language'] = LANGUAGE
+        city = city.replace(' ', '+')
+        html_content = session.get(
+            f'https://www.google.com/search?client=firefox-b-d&q=weather+{city}').text
+        return html_content
+
+
 def home(request):
+     if 'ci ty' in request.GET:
+         city = request.GET.get('city')
+          html_content = get_html_content(city)
+        from bs4 import BeautifulSoup
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+         pass
     return render(request, 'core/home.html')
