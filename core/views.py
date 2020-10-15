dbec9df369f2e53988a03af32e4f63ad03b3426e
Pooja Kelkar <poojakelkar53@gmail.com>
2020-10-15 21:40:48 +0530

    getting data from google

--- core/views.py
+++ core/views.py
@@ -19,11 +19,20 @@ def get_html_content():
 
 
 def home(request):
-     if 'ci ty' in request.GET:
+    weather_data = None
+     if 'city' in request.GET:
          city = request.GET.get('city')
           html_content = get_html_content(city)
         from bs4 import BeautifulSoup
         soup = BeautifulSoup(html_content, 'html.parser')
 
+        weather_data=dict()
+        weather_data['region']= soup.find('div', attrs={'id': 'wob_loc'}).text
+        weather_data['daytime']= soup.find('div', attrs={'id': 'wob_dts'}).text
+        weather_data['status']= soup.find('span', attrs={'id': 'wob_dc'}).text
+        weather_data['temp']= soup.find('span', attrs={'id': 'wob_tm'}).text
+
+        
+
          pass
-    return render(request, 'core/home.html')
+    return render(request, 'core/home.html',{'weather':weather_data})
